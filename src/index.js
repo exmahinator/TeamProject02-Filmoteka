ebb32223590b017fa5beea050bfe75b5bb6881eb
Vita Koval <mayevit@hotmail.com>
2022-09-06 19:01:35 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import { paginationTablet } from './js/pagination.js';
+
+import { onItemClick } from './js/onItemClick.js';
+import { pagination } from './js/pagination.js';
+
+pagination.on('beforeMove', function (eventData) {
+  console.log(eventData.page);
+  // Тут повинна бути функція перемальовування сторінки з інформації з бекенда. Значення сторінки: eventData.page
+});
+
 import './js/search'
+
+
