34694ee668ecdd54d84184f62685425f8ddf9a16
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-09-06 14:46:56 +0300

    Update Paginator

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import { paginationTablet } from './js/pagination.js';
+import { pagination } from './js/pagination.js';
+
+pagination.on('beforeMove', function (eventData) {
+  console.log(eventData.page);
+  // Тут повинна бути функція перемальовування сторінки з інформації з бекенда. Значення сторінки: eventData.page
+});
