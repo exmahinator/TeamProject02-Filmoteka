cc3a8983f00857e4df367ba58dcd170fa600a74a
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-09-06 19:39:32 +0300

    Update team modal + paginator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import { pagination } from './js/pagination.js';
 
-pagination.on('beforeMove', function (eventData) {
-  console.log(eventData.page);
-  // Тут повинна бути функція перемальовування сторінки з інформації з бекенда. Значення сторінки: eventData.page
-});
+// pagination.on('beforeMove', function (eventData) {
+//   console.log(eventData.page);
+//   // Тут повинна бути функція перемальовування сторінки з інформації з бекенда. Значення сторінки: eventData.page
+// });
+
+const teamRefSection = document.querySelector('.team');
+const teamRefBackdrop = document.querySelector('.footer__btn');
+const teamRefClose = document.querySelector('.team__subitem--central__top');
+
+teamRefBackdrop.addEventListener('click', openTeam);
+
+function openTeam(event) {
+  event.preventDefault();
+  console.log('clicking the button');
+
+  teamRefSection.classList.toggle('team-hidden');
+  teamRefClose.addEventListener('click', closeTeamModal);
+}
+
+function closeTeamModal() {
+  teamRefSection.classList.toggle('team-hidden');
+  teamRefClose.removeEventListener('click', closeTeamModal);
+}
