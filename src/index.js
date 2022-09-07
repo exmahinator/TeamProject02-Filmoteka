3d4b7aa0f3013b0900fd2a084d786710755ad9c6
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-09-07 13:51:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import './js/service-themoviedb-api';
-import './js/ourTeam';
-import './js/create-markup_gallery';
-
-import './js/header-library-btn';
-
 import './js/search';
 import './js/onItemClick';
 
+import './js/service-themoviedb-api';
+import './js/ourTeam';
+
+import './js/header-library-btn';
+import './js/create-markup_gallery';
